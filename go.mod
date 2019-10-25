dcfe4dbacd3dc338254f32b27991927e355864e3
sho <sskmy1024.y@gmail.com>
2019-10-25 16:31:27 +0900

    add double request paeser

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yuuis/RecommendSystem
 go 1.13
 
 require (
+	github.com/davecgh/go-spew v1.1.0
 	github.com/gin-gonic/gin v1.4.0
 	github.com/google/uuid v1.1.1
 	github.com/joho/godotenv v1.3.0
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,13 +11,37 @@ import (
 	"github.com/yuuis/RecommendSystem/infrastructures"
 )
 
-func TestHelloSuccess(t *testing.T) {
+// func TestHelloSuccess(t *testing.T) {
+
+// 	s := infrastructures.NewServer()
+// 	api.Router(s)
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("GET", "/api/hello", nil)
+// 	s.ServeHTTP(w, req)
+
+// 	json := `{"Message":"hello"}`
+// 	assert.Equal(t, 200, w.Code)
+// 	assert.Equal(t, json, w.Body.String())
+// }
+
+func TestServiceRequestToHotpepper(t *testing.T) {
+	infrastructures.InitEnvironment()
 
 	s := infrastructures.NewServer()
 	api.Router(s)
 
+	requestData := `
+	{
+		"topic_category": 2,
+		"require_service": true,
+		"service_data_value": {
+			"keywords": "寿司"
+		}
+	}`
+
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/api/hello", nil)
+	req, _ := http.NewRequest("POST", "/api/recommend", bytes.NewBuffer([]byte(requestData)))
 	s.ServeHTTP(w, req)
 
 	json := `{"Message":"hello"}`
