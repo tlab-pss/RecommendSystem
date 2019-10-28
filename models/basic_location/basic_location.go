b551b98de788e069dc9ace803dd8153e19a49f2a
yu ishiguro <yuuis.egr@gmail.com>
2019-10-29 01:05:44 +0900

    add struct and method for basic_location

---
+++ models/basic_location/basic_location.go
@@ -0,0 +1,12 @@
+package basic_location
+
+import (
+	"github.com/yuuis/RecommendSystem/models/location"
+)
+
+type BasicLocation struct {
+	ID        string  `json:"ID"`
+	House     location.Location `json:"House"`
+	Office    location.Location `json:"Office"`
+	//Route     []location.Location `json:"Route"` todo: 未実装
+}
--- modules/hotpepper/optimize.go
+++
@@ -1,9 +0,0 @@
-package hotpepper
-
-func Optimize() (*Condition, error) {
-	// todo: 位置情報とか食べたものから、hotpepper用のconditionを作って返す
-	return &Condition{}, nil
-}
-
-type Condition struct {
-}
